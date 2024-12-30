374a6ee7687f4d161a57cde3e85310c0317f7bc9
AD2L7ER <69653233+AD2L7ER@users.noreply.github.com>
2024-12-30 07:48:06 +0330

    install.sh

--- install.sh
+++ install.sh
@@ -114,7 +114,7 @@ fix_abuse() {
     confirmation_menu || return
     echo -e "[1;32mExecuting hetzner fix abuse...[0m"
     echo "Activating UFW..."
-    sudo ufw --force enable
+    sudo ufw --force enable > /dev/null 2>&1
     confirmation_menu || return
     echo -e "\033[1;32mExecuting hetzner fix abuse...\033[0m"
     sudo ufw enable
